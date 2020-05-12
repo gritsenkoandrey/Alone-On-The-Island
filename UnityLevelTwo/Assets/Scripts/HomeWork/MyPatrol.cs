69f5012a3d2932dff0e90edf348675f8633477b2
Андрей Гриценко <a.gritsenko87@yandex.ru>
2020-05-13 00:53:52 +0300

    Добавил свой алгоритм патрулирования

---
+++ UnityLevelTwo/Assets/Scripts/HomeWork/MyPatrol.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+
+public class MyPatrol : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5.0f;
+    private float _obstacleRange = 2.0f;
+    private bool _isAlive;
+
+    private void Start()
+    {
+        _isAlive = true;
+    }
+
+    private void Update()
+    {
+        if (_isAlive)
+        {
+            transform.Translate(0, 0, _speed * Time.deltaTime);
+        }
+        var ray = new Ray(transform.position, transform.forward);
+        if (Physics.SphereCast(ray, 1.0f, out var hit))
+        {
+            GameObject hitObject = hit.transform.gameObject;
+
+            if (hit.distance < _obstacleRange)
+            {
+                float angle = Random.Range(-120, 120);
+                transform.Rotate(0, angle, 0);
+            }
+        }
+    }
+}
